a16585da572e3cb550ebeb9cb5bc217450f40e05
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-12-02 16:08:21 +0530

    Added Firebase to Project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../firebase-config";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const app = initializeApp(firebaseConfig);
+
+  return <Component {...pageProps} />;
 }
