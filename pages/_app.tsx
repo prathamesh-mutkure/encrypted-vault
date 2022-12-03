42481640a59ed432a39e26c53e6069af93938878
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-12-03 20:09:17 +0530

    Added Providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,29 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "../firebase-config";
+import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { Provider } from "react-redux";
+import store from "../store/index";
+
+const theme = createTheme({});
 
 export default function App({ Component, pageProps }: AppProps) {
   const app = initializeApp(firebaseConfig);
 
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={pageProps?.session}>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <title>Anti-Cheat Exam App</title>
+            <meta name="author" content="Prathamesh Mutkure" />
+          </Head>
+          <Component {...pageProps} />
+          {/* <ToastContainer position="bottom-center" theme="light" /> */}
+        </ThemeProvider>
+      </Provider>
+    </SessionProvider>
+  );
 }
