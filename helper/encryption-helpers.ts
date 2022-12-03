cc33daf5e348f73ccabade2a0aec9ac13e75dd67
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-12-03 17:34:40 +0530

    Added RSA Algo Helpers

---
+++ helper/encryption-helpers.ts
@@ -0,0 +1,44 @@
+import * as crypto from "crypto";
+
+// Generate a new RSA key pair
+export const getKeys = () => {
+  const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 2048, // the length of the key in bits
+    publicKeyEncoding: {
+      type: "spki", // the type of encoding used for the public key
+      format: "pem", // the format of the key
+    },
+    privateKeyEncoding: {
+      type: "pkcs8", // the type of encoding used for the private key
+      format: "pem", // the format of the key
+    },
+  });
+
+  return { publicKey, privateKey };
+};
+
+const encryptMessage = (message: string, publicKey: string) => {
+  // Encrypt the message using the public key
+  const encrypted = crypto.publicEncrypt(
+    {
+      key: publicKey,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+    },
+    Buffer.from(message)
+  );
+
+  return encrypted.toString();
+};
+
+const decryptMessage = (encrypted: string, privateKey: string) => {
+  // Decrypt the message using the private key
+  const decrypted = crypto.privateDecrypt(
+    {
+      key: privateKey,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+    },
+    Buffer.from(encrypted)
+  );
+
+  decrypted.toString(); // 'This is a secret message'
+};
