73ac581e86ee6e432d72f359bfe4d933ff14e5be
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-12-05 02:03:50 +0530

    RSA to AES

--- helper/encryption-helpers.ts
+++ helper/encryption-helpers.ts
@@ -1,5 +1,7 @@
 import * as crypto from "crypto";
 
+const algorithm = "aes256"; // or any other algorithm supported by OpenSSL
+
 // Generate a new RSA key pair
 export const getKeys = () => {
   const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
@@ -14,31 +16,43 @@ export const getKeys = () => {
     },
   });
 
-  return { publicKey, privateKey };
+  // return { publicKey, privateKey };
+  return { publicKey, privateKey: publicKey };
 };
 
 export const encryptMessage = (message: string, publicKey: string) => {
   // Encrypt the message using the public key
-  const encrypted = crypto.publicEncrypt(
-    {
-      key: publicKey,
-      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-    },
-    Buffer.from(message)
-  );
+  // const encrypted = crypto.publicEncrypt(
+  //   {
+  //     key: publicKey,
+  //     padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+  //   },
+  //   Buffer.from(message)
+  // );
 
-  return encrypted.toString();
+  // return encrypted.toString();
+
+  const cipher = crypto.createCipher(algorithm, publicKey);
+  const encrypted = cipher.update(message, "utf8", "hex") + cipher.final("hex");
+
+  return encrypted;
 };
 
 export const decryptMessage = (encrypted: string, privateKey: string) => {
   // Decrypt the message using the private key
-  const decrypted = crypto.privateDecrypt(
-    {
-      key: privateKey,
-      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-    },
-    Buffer.from(encrypted)
-  );
+  // const decrypted = crypto.privateDecrypt(
+  //   {
+  //     key: privateKey,
+  //     padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+  //   },
+  //   Buffer.from(encrypted)
+  // );
 
-  decrypted.toString(); // 'This is a secret message'
+  // decrypted.toString(); // 'This is a secret message'
+
+  var decipher = crypto.createDecipher(algorithm, privateKey);
+  var decrypted =
+    decipher.update(encrypted, "hex", "utf8") + decipher.final("utf8");
+
+  return decrypted;
 };
