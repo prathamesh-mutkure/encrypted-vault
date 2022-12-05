4383cce29b3985b891fee953b65e7fb8ae7b73b1
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-12-05 10:57:17 +0530

    Added Firebase Config

---
+++ firebase-config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDEOOjKU89jGKF4rSqvSAe_7dBUVeEZfkU",
+  authDomain: "encrypted-vault-bf545.firebaseapp.com",
+  projectId: "encrypted-vault-bf545",
+  storageBucket: "encrypted-vault-bf545.appspot.com",
+  messagingSenderId: "895351647698",
+  appId: "1:895351647698:web:1d928572c742de6b676c0b",
+};
